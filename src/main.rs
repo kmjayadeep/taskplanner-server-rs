220507d0c8915e97a92b96f3ec7e42e35b06ae41
Jayadeep KM <kmjayadeep@gmail.com>
2025-02-17 23:54:25 +0100

    init api
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(index));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap()
+}
+
+async fn index() -> &'static str {
+    "Hello world"
 }
